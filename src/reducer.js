5f5235148edd413674c6829a9df45c699fd9ec1f
broncs <jhowfrombrazil@gmail.com>
2021-01-01 13:23:10 -0300

    Clear cart, remove, increase/decrease amount from items

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,35 @@
 const reducer = (state, action) => {
-  return state;
+  switch (action.type) {
+    case "CLEAR_CART":
+      return { ...state, cart: [] };
+    case "REMOVE":
+      return {
+        ...state,
+        cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+      };
+    case "INCREASE":
+      let tempCartIncrease = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount + 1 };
+        }
+        return cartItem;
+      });
+      return { ...state, cart: tempCartIncrease };
+    case "DECREASE":
+      let tempCartDecrease = state.cart
+        .map((cartItem) => {
+          if (cartItem.id === action.payload) {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.amount !== 0);
+
+      return { ...state, cart: tempCartDecrease };
+
+    default:
+      throw new Error("hello");
+  }
 };
 
 export default reducer;
